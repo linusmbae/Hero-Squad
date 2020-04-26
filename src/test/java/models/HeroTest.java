c769468b3200ca21e966e3fcd81c874959800432
Linus Mutugi <linusmutugi5178@gmail.com>
2020-04-26 15:31:26 +0300

    heroIsCreatedCorrectly test passes

---
+++ src/test/java/models/HeroTest.java
@@ -0,0 +1,19 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HeroTest {
+    @Test
+    public void heroIsCreatedCorrectly_true() throws Exception
+    {
+        Hero hero=createNewHero();
+        assertEquals(true,hero instanceof Hero);
+    }
+
+public Hero createNewHero()
+{
+    return new Hero("Super Girl",35,"Flying", "stones",1);
+}
+}
