e08bdf2812a6448ae0341e83369710809c4bf6f5
Linus Mutugi <linusmutugi5178@gmail.com>
2020-04-26 17:00:25 +0300

    object squad created

---
+++ src/main/java/models/Squad.java
@@ -0,0 +1,14 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Squad {
+    private static ArrayList<Squad> instances = new ArrayList<>();
+    private int id;
+    private int maxSize;
+    private String name;
+    private String cause;
+
+
+
+}
