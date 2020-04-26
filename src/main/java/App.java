e017e65c0215f39b6a4f35f13eb1322c05b3f33a
Linus Mutugi <linusmutugi5178@gmail.com>
2020-04-26 21:37:22 +0300

    layout and index templates complete

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,22 @@
+import models.Hero;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+
+        get("/",(request, response) ->
+        {
+            Map<String, Object>model=new HashMap<String, Object>();
+            ArrayList<Hero>heroes=Hero.getAll();
+            model.put("heroes",heroes);
+            return new ModelAndView(model,"index.hbs");
+        },new HandlebarsTemplateEngine());
     }
 }
