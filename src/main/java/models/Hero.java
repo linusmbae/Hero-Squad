debb61949958d74815fcedae0fadef8ad96181b1
Linus Mutugi <linusmutugi5178@gmail.com>
2020-04-26 15:24:59 +0300

    equals and hashcode complete

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.Objects;
+
 public class Hero {
     private String name;
     private int age;
@@ -15,4 +17,20 @@ public class Hero {
         this.id = id;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Hero hero = (Hero) o;
+        return age == hero.age &&
+                id == hero.id &&
+                Objects.equals(name, hero.name) &&
+                Objects.equals(specialPowers, hero.specialPowers) &&
+                Objects.equals(weakness, hero.weakness);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, specialPowers, weakness, id);
+    }
 }
