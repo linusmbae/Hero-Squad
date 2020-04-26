e842b7a4af3929dd376f1bfe1558627731ff25f5
Linus Mutugi <linusmutugi5178@gmail.com>
2020-04-26 16:10:40 +0300

    test AllHeroesContainsAllHeroes passes

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,10 +1,20 @@
 package models;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class HeroTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Hero.clearAllTasks(); // clear out all the tasks before each test
+    }
     @Test
     public void heroIsCreatedCorrectly_true() throws Exception
     {
@@ -23,7 +33,15 @@ public class HeroTest {
     public void HeroesAreCorrectlyReturned_true() throws Exception {
         Hero hero = createNewHero();
         Hero otherHero = new Hero("Super Man",45,"super Speed", "stones",2);
-        assertEquals(3, Hero.getAll().size());
+        assertEquals(2, Hero.getAll().size());
+    }
+
+    @Test
+    public void AllHeroesContainsAllHeroes_true() throws Exception {
+        Hero hero = createNewHero();
+        Hero otherHero = new Hero("Super Man",45,"super Speed", "stones",2);
+        assertTrue(Hero.getAll().contains(hero));
+        assertTrue(Hero.getAll().contains(otherHero));
     }
 
 public Hero createNewHero()
