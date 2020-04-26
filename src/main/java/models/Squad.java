1ce5f583fbfd2efc705327a882d23fa42b08374c
Linus Mutugi <linusmutugi5178@gmail.com>
2020-04-26 17:02:59 +0300

    Generate getters and setters

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -16,5 +16,43 @@ public class Squad {
         this.cause = cause;
     }
 
+    public static ArrayList<Squad> getAll() {
+        return instances;
+    }
 
+    public static void setAll(ArrayList<Squad> instances) {
+        Squad.instances = instances;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
 }
