758db99c0fcd42578ee3c62118e224ffdfcc44fb
Linus Mutugi <linusmutugi5178@gmail.com>
2020-04-26 15:24:18 +0300

    constructor complete

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,18 @@
 package models;
 
 public class Hero {
+    private String name;
+    private int age;
+    private String specialPowers;
+    private String weakness;
+    private int id;
+
+    public Hero(String name, int age, String specialPowers, String weakness, int id) {
+        this.name = name;
+        this.age = age;
+        this.specialPowers = specialPowers;
+        this.weakness = weakness;
+        this.id = id;
+    }
+
 }
