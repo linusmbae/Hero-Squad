b00da05164beb613d527c302abb4748f2bd129be
Linus Mutugi <linusmutugi5178@gmail.com>
2020-04-27 07:26:04 +0300

    squad and hero forms complete

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
+import javafx.geometry.Pos;
 import models.Hero;
+import models.Squad;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -18,5 +20,42 @@ public class App {
             model.put("heroes",heroes);
             return new ModelAndView(model,"index.hbs");
         },new HandlebarsTemplateEngine());
+
+        get("/heroes/new",(request, response) ->
+        {
+            Map<String, Object>model=new HashMap<String, Object>();
+            return new ModelAndView(model,"new-heroes-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+//        post("/heroes/new",(request, response) ->
+//        {
+//            Map<String, Object>model = new HashMap<String, Object>();
+//            String name=request.queryParams("name");
+//            int age = Integer.parseInt(request.queryParams("age"));
+//            String specialPowers=request.queryParams("specialPowers");
+//            String weakness = request.queryParams("weakness");
+//            int id =Integer.parseInt(request.queryParams("id"));
+//            Hero newHero=new Hero(name,age,specialPowers,weakness,id);
+//            model.put("hero",newHero);
+//            response.redirect("/");
+//        }, new HandlebarsTemplateEngine());
+
+        get("/squad/new",(request, response) ->
+        {
+            Map<String, Object>model=new HashMap<String, Object>();
+            return new ModelAndView(model, "new-squad-form.hbs");
+        },new HandlebarsTemplateEngine());
+//        post("/squad/new", (request, response) ->
+//        {
+//            Map<String, Object>model=new HashMap<String, Object>();
+//            String name= request.queryParams("name");
+//            String cause=request.queryParams("cause");
+//            int id = Integer.parseInt(request.queryParams("id"));
+//            Squad newSquad=new Squad(id,name,cause);
+//            model.put("squad",newSquad);
+//            response.redirect("/");
+//        },new HandlebarsTemplateEngine());
+
+
     }
 }
